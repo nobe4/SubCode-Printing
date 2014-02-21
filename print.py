6ea7f6d2bb6cbf03520ce834abd03affade925c7
nobe4 <haffreinguev@gmail.com>
2014-02-21 11:48:55 +0100

    working version tested on linux/OS X

--- print.py
+++ print.py
@@ -1,26 +1,30 @@
 import sublime, sublime_plugin
 
-# class CreateNewWindowCommand(sublime_plugin.WindowCommand):
-#     def run(self):
-#         s = self.window.active_view().substr(sublime.Region(0, self.window.active_view().size()))
-#         newFile = self.window.new_file()
-#         newFile.run_command("copy_text",{"textBuffer" : s})
-
-# class CopyTextCommand(sublime_plugin.TextCommand):
-#     def printAChar(self,char,edit):
-#         self.view.insert(edit, self.view.size(), char)
-
-#     def run(self, edit, textBuffer):
-#         s = list(textBuffer)
-#         for i in s:
-#             self.printAChar(i, edit)
-
-
-
-
 class PrintCodeCommand(sublime_plugin.WindowCommand):
     def run(self):
-        # self.window.run_command("create_new_window")
-        def makeTimeout(time, text): sublime.set_timeout(lambda : print(text),time)
-        for x in range(1,10):
-            makeTimeout(x*1000,x)
+        syntax = self.window.active_view().settings().get('syntax')
+        allString = self.window.active_view().substr(sublime.Region(0, self.window.active_view().size()))
+        newFile = self.window.new_file()
+        newFile.set_syntax_file(syntax)
+        t = 0 # timer
+        a = "add" # add, remove, clear
+        for c in  allString:
+            if c == 'é':
+                a = "remove"
+            elif c == 'è':
+                a = "clear"
+            else : 
+                a = "add"
+            sublime.set_timeout(lambda char = c, action = a : newFile.run_command("process_char_action",{ "action" : action , "char" : char}) , t*100)
+            t += 1
+
+class ProcessCharActionCommand(sublime_plugin.TextCommand):
+    def run(self, edit, action, char):
+        if action == "add":
+            self.view.insert(edit, self.view.size(), char)
+        elif action == "remove":
+            region = sublime.Region(self.view.size()-1, self.view.size())
+            self.view.erase(edit, region)
+        elif action == "clear" :
+            region = sublime.Region(0, self.view.size())
+            self.view.erase(edit, region)
